fdf851483abd2eebb37bcf765fc1722f2a910e36
MD. Raisul Islam Niloy <raisulislamniloy47@gmail.com>
2026-04-18 22:11:46 +0600

    Init

---
+++ src/main/java/com/example/git_and_github/module_19_assignment/OstadJavaSpringBoot3Module19AssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.git_and_github.module_19_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OstadJavaSpringBoot3Module19AssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OstadJavaSpringBoot3Module19AssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/git_and_github/module_19_assignment/OstadJavaSpringBoot3Module19AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git_and_github.module_19_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OstadJavaSpringBoot3Module19AssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
